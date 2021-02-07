b7d440873c8be759b1c45e4b323fed241df7985b
P. Taylor Goetz <ptgoetz@apache.org>
2021-02-06 22:35:36 -0500

    Cosmetic changes to prefs panel

--- CocoaFIBS/AGFIBSLoginWindowController.m
+++ CocoaFIBS/AGFIBSLoginWindowController.m
@@ -176,7 +176,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 - (void)windowDidLoad
 {
-	[[self loginWindow] setFrameAutosaveName:@"LoginWindow"];	
+	[[self loginWindow] setFrameAutosaveName:@"LoginWindow"];
+//    [[self loginWindow] ]
 }
 
 @end
